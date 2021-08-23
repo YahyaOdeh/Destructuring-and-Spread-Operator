8b791a29bffc0a3796985c6cc1f26574dd692b4b
Yahya Mustafa <yahya_odeh@outlook.com>
2021-08-23 16:03:41 +0300

    first commit

---
+++ question1.js
@@ -0,0 +1,11 @@
+let y = 'x';
+let x = 'y';
+console.log(`before x value is ${x} y value is ${y} `)
+
+// swap x and y values using Destructuring
+
+let [nx, ny] = [y, x]
+
+// your code here
+console.log(`after x value is ${nx} y value is ${ny} `)
+
---
+++ question4.js
@@ -0,0 +1,43 @@
+const player = {
+    name:'hatem',
+    attributes:{
+        attack:{
+            speed:60,
+            shooting:95,
+        },
+        defense:{
+            tackle:80,
+            interception:66
+        }
+    },
+    overall:64,
+    age:23,
+}
+
+// use Destructuring to get name,attack attributes only when
+
+const {name,attributes:{attack}} = player;
+
+console.log(name,attack) // hatem { speed: 60, shooting: 95 }
+
+////////////////////////////////////////////////////////
+
+const mid = {shortPass:80,longPass:67}
+
+const newPlayer = {...player, mid}
+
+console.log(newPlayer) 
+
+// update this function to return the type is other if no parameters passed
+const fn = type => `the type is ${type}` 
+
+// you can't touch this line
+console.log(fn()) 
+
+// note you can solve this question using both arrow and normal function
+
+/**
+ * 
+ * you cann't use 
+ * const attack = player.attributes.attack; ❌ 
+ */
